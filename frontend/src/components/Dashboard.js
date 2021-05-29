1ad349c9908a2a6510744ae2785a99d01e059ac5
Khuldoonqataesheh <kqataesheh@gmail.com>
2021-05-29 17:51:35 +0300

    getAllArticles without style

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,6 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Dashboard() {
-  return <div>dashboard</div>;
+  const [status, setStatus] = useState(false);
+  const [articles, setArticles] = useState(undefined);
+ 
+  const getAllArticle = () => {
+    axios
+      .get(`http://localhost:5000/articles`)
+      .then((res) => {
+        setArticles(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getAllArticle();
+  }, [status]);
+
+  return (
+    <div>
+      <p>dashboard</p>
+      <button
+        onClick={() => {
+            setStatus();
+        }}
+      >
+        Get All Articles
+      </button>
+   {articles && articles.map((elem, i) =>  <div key={i}><h1> {elem.title} </h1> <p>{elem.description}</p> </div>)}
+    </div>
+  );
 }
